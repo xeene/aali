5c2aa6e06ab678e3f46ef933e08554c112deea41
xeene <xeene_@naver.com>
2023-08-31 11:24:03 +0900

    animation timer faster

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -130,7 +130,7 @@ export default {
     parallax();
     const interval = setInterval(() => {
       this.text = this.text < 3 ? this.text + 1 : 1;
-    }, 5000);
+    }, 2000);
     return () => clearInterval(interval);
   },
 };
